0d130e22d9048852bdcbd20d65df005f9c067740
jclayton128 <jclayton128@gmail.com>
2021-03-23 22:15:26 -0400

    Nice-feeling 8-way WASD tank movement

--- Assets/TankMovement.cs
+++ Assets/TankMovement.cs
@@ -9,8 +9,13 @@ public class TankMovement : Movement
 
     //param
     public float moveSpeed = 1.0f;
-    public float turnSpeed = 30f; //deg per second
+    public float turnSpeed = 360f; //deg/sec
 
+    //hood
+    public Vector3 commandedVector = new Vector3();
+    float maxAngleOffBoresightToDrive = 10f;
+    bool isCommandedToMove = false;
+    float angleOffCommandedVector;
 
     protected override void Start()
     {
@@ -20,11 +25,67 @@ public class TankMovement : Movement
     // Update is called once per frame
     void Update()
     {
-        RespondToMovementCommand();
+        Debug.DrawLine(transform.position, commandedVector.normalized + transform.position, Color.blue);
+        CheckForCommandedMovement();
+        UpdateCommandedVectorAndAngleOffIt();
+        RotateToCommandedVector();
+        DriveAlongCommandedVector();
     }
 
-    private void RespondToMovementCommand()
+    private void CheckForCommandedMovement()
     {
-        rb.velocity = new Vector2(cs.horizComponent, cs.vertComponent) * moveSpeed;
+        if (Mathf.Abs(cs.horizComponent) > 0 || Mathf.Abs(cs.vertComponent) > 0) //if move commands are non-zero;
+        {
+            isCommandedToMove = true;
+        }
+        if (Mathf.Abs(cs.horizComponent) == 0f && Mathf.Abs(cs.vertComponent) == 0f) //if move commands are non-zero;
+        {
+            isCommandedToMove = false;
+        }
+    }
+
+    private void DriveAlongCommandedVector()
+    {
+        if (!isCommandedToMove || Mathf.Abs(angleOffCommandedVector) > maxAngleOffBoresightToDrive)
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, Time.deltaTime*3);
+            return;
+        }
+        if (isCommandedToMove)
+        {
+            if (Mathf.Abs(angleOffCommandedVector) < maxAngleOffBoresightToDrive * 4)
+            {
+                rb.velocity = commandedVector * (moveSpeed / 2);
+            }
+            if (Mathf.Abs(angleOffCommandedVector) < maxAngleOffBoresightToDrive)
+            {
+                rb.velocity = commandedVector * moveSpeed;
+            }
+        }
+    }
+
+    private void UpdateCommandedVectorAndAngleOffIt()
+    {
+        commandedVector.x = cs.horizComponent;
+        commandedVector.y = cs.vertComponent;
+        commandedVector.Normalize();
+        angleOffCommandedVector = Vector3.SignedAngle(transform.up, commandedVector, Vector3.forward);
+    }
+
+    private void RotateToCommandedVector()
+    {
+        if (!isCommandedToMove)
+        {
+            rb.angularVelocity = 0;
+            return;
+        }
+        if (angleOffCommandedVector > -0.1f)
+        {
+            rb.angularVelocity = turnSpeed;
+        }
+        if (angleOffCommandedVector < 0.1f)
+        {
+            rb.angularVelocity = -turnSpeed;
+        }
     }
 }
