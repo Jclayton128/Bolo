ad9ccf6250fe3ed3ca33e3a96c29eb531dd2844a
jclayton128 <jclayton128@gmail.com>
2021-03-28 15:33:55 -0400

    House object with multiple random sprites

---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class House : MonoBehaviour
+{
+    //init
+    SpriteRenderer sr;
+    [SerializeField] Sprite[] possibleHouseSprites = null;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        ChooseHouseImage();
+    }
+
+    private void ChooseHouseImage()
+    {
+        int rand = UnityEngine.Random.Range(0, possibleHouseSprites.Length);
+        sr.sprite = possibleHouseSprites[rand];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
