499db9aa2a5432a8705cc30ad35b60b50b1b5a09
jclayton128 <jclayton128@gmail.com>
2021-03-31 14:09:18 -0400

    Compass size scale changes based on distance to nearest City Square

--- Assets/Scripts/Compass.cs
+++ Assets/Scripts/Compass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class Compass : MonoBehaviour
 {
@@ -10,24 +11,45 @@ public class Compass : MonoBehaviour
     CityManager cm;
     GameObject player;
     TextMeshProUGUI cityNameTextBar;
+    float maxCityDistance = 30f;
 
     //hood
     CitySquare nearestCS;
+    float distToNearestCS;
+    float initialScale;
+
     void Start()
     {
         cm = FindObjectOfType<CityManager>();
         player = GameObject.FindGameObjectWithTag("Player");
         cityNameTextBar = GameObject.FindGameObjectWithTag("CityNameTextBar").GetComponent<TextMeshProUGUI>();
+        initialScale = transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        OrientCompassTowardsNearestCity();
+        AdjustCompassSizeBasedOnDistanceToNearestCity();
+
+    }
+
+    private void AdjustCompassSizeBasedOnDistanceToNearestCity()
+    {
+        float dist = (nearestCS.transform.position - player.transform.position).magnitude;
+        float factor = 1 - (dist / maxCityDistance);
+
+        factor = Mathf.Clamp(factor, .3f, 1.0f);
+        Debug.Log("post clamp factor: " + factor);
+        transform.localScale = Vector3.one * factor * initialScale;
+    }
+
+    private void OrientCompassTowardsNearestCity()
+    {
         nearestCS = cm.FindNearestCitySquare(player.transform);
         cityNameTextBar.text = nearestCS.cityName;
         float ang = cm.FindAngleToCitySquare(player.transform, nearestCS);
-        Quaternion rot = Quaternion.Euler(0,0,ang);
+        Quaternion rot = Quaternion.Euler(0, 0, ang);
         transform.rotation = rot;
-
     }
 }
