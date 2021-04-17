2f1e38c36d731afc81b872a9d355b07027690bcd
jclayton128 <jclayton128@gmail.com>
2021-04-17 15:11:20 -0400

    StealthHider size based on speed and terrain.

--- Assets/Scripts/Units/ControlSource.cs
+++ Assets/Scripts/Units/ControlSource.cs
@@ -122,4 +122,22 @@ public abstract class ControlSource : MonoBehaviour
 
     }
 
+    public int GetCurrentTerrainType()
+    {
+        NavMeshHit hit;
+        NavMesh.SamplePosition(transform.position, out hit, 0.1f, NavMesh.AllAreas);
+        int index = IndexFromMask(hit.mask);
+        return index;
+    }
+
+    private int IndexFromMask(int mask)
+    {
+        for (int i = 0; i < 32; ++i)
+        {
+            if ((1 << i) == mask)
+                return i;
+        }
+        return -1;
+    }
 }
+
--- Assets/Scripts/Units/StealthHider.cs
+++ Assets/Scripts/Units/StealthHider.cs
@@ -1,24 +1,98 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(CircleCollider2D))]
+
 
 public class StealthHider : MonoBehaviour
 {
     //init
     SpriteRenderer[] srs;
-    
+    Rigidbody2D rb;
+    CircleCollider2D hiderColl;
+    ControlSource cs;
+
+    //param
+    public float hiderRadius_Base;
+    public float hiderGrowthRate = .5f; //per second;
+    public float hiderShrinkRate = .2f; //per second;
+
+    //hood
+    float hiderRadius_Modified;
+    public float hiderRadius_TerrainModifier = 1;
+
 
     // Start is called before the first frame update
     void Start()
     {
         srs = transform.root.GetComponentsInChildren<SpriteRenderer>();
         MakeObjectInvisible();
+        rb = transform.root.GetComponentInChildren<Rigidbody2D>();
+        hiderColl = GetComponent<CircleCollider2D>();
+        hiderColl.radius = hiderRadius_Base;
+        cs = transform.root.GetComponentInChildren<ControlSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateHiderRadiusInputBasedOnSpeed();
 
+    }
+    private void UpdateHiderRadiusInputBasedOnSpeed()
+    {
+        float vel = rb.velocity.magnitude;
+        hiderRadius_TerrainModifier = GetTerrainModifier();
+        hiderRadius_Modified = hiderRadius_Base * vel * hiderRadius_TerrainModifier;
+        AdjustHiderRadius();
+    }
+
+    private float GetTerrainModifier()
+    {
+        int terrain = cs.GetCurrentTerrainType();
+
+        if (terrain == 4) //road
+        {
+            hiderRadius_TerrainModifier = 1.25f;
+            Debug.Log("hiding in road");
+            return hiderRadius_TerrainModifier;
+        }
+        if (terrain == 5)  //hills
+        {
+            hiderRadius_TerrainModifier = .5f;
+            Debug.Log("hiding in hills");
+            return hiderRadius_TerrainModifier;
+        }
+        if (terrain == 6) //forest
+        {
+            hiderRadius_TerrainModifier = .75f;
+            Debug.Log("hiding in forest");
+            return hiderRadius_TerrainModifier;
+        }
+        else
+        {
+            hiderRadius_TerrainModifier = 1;
+            Debug.Log("hiding in field");
+            return hiderRadius_TerrainModifier;
+        }
+
+
+    }
+
+    private void AdjustHiderRadius()
+    {
+        if (hiderRadius_Modified > hiderColl.radius)
+        {
+            Debug.Log("hider radius needs to grow");
+            hiderColl.radius += hiderGrowthRate * Time.deltaTime;
+        }
+        if (hiderRadius_Modified < hiderColl.radius)
+        {
+            Debug.Log("hider radius needs to shrink");
+            hiderColl.radius -= hiderShrinkRate * Time.deltaTime;
+        }
+        hiderColl.radius = Mathf.Clamp(hiderColl.radius, hiderRadius_Base / 4, hiderRadius_Base * 4);
     }
 
     public void MakeObjectInvisible()
