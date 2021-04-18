3b6084af800d9b05578c2ad0669c096cd6802f72
jclayton128 <jclayton128@gmail.com>
2021-04-18 17:23:20 -0400

    Improved Radar

---
+++ Assets/RadarSector.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadarSector : MonoBehaviour
+{
+    //init
+    [SerializeField] Image[] dotLevels = null;
+
+    //param
+    Color myGreen = new Color(0.2195399f, 0.95f, .2134212f);
+
+    //hood
+    float intensityActual;
+    public float intensityTarget;
+    public float fadeRate;
+    public float riseRate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetAllDotsToZero();
+    }
+
+    private void SetAllDotsToZero()
+    {
+        foreach (Image dot in dotLevels)
+        {
+            dot.color = new Color(0.2195399f, 0.95f, .2134212f, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrainActualIntensityToMatchTargetIntensity();
+        IlluminateDotsBasedOnIntensity();
+    }
+
+    private void DrainActualIntensityToMatchTargetIntensity()
+    {
+        if (intensityActual < intensityTarget)
+        {
+            intensityActual = Mathf.MoveTowards(intensityActual, intensityTarget, riseRate * Time.deltaTime);
+        }
+        if (intensityActual > intensityTarget)
+        {
+            intensityActual = Mathf.MoveTowards(intensityActual, intensityTarget, fadeRate * Time.deltaTime);
+        }
+
+
+    }
+
+    private void IlluminateDotsBasedOnIntensity()
+    {
+        float alpha_0 = (intensityActual - 0) / .25f;
+        float alpha_1 = (intensityActual - .25f) / .25f;
+        float alpha_2 = (intensityActual - .5f) / .25f;
+        float alpha_3 = (intensityActual - .75f) / .25f;
+        dotLevels[0].color = new Color(0.2195399f, 0.95f, .2134212f, alpha_0);
+        dotLevels[1].color = new Color(0.2195399f, 0.95f, .2134212f, alpha_1);
+        dotLevels[2].color = new Color(0.2195399f, 0.95f, .2134212f, alpha_2);
+        dotLevels[3].color = new Color(0.2195399f, 0.95f, .2134212f, alpha_3);
+    }
+}
--- Assets/Scripts/UI/RadarScreen.cs
+++ Assets/Scripts/UI/RadarScreen.cs
@@ -9,16 +9,18 @@ public class RadarScreen : MonoBehaviour
     //init
     GameObject player;
     UnitTracker ut;
-    [SerializeField] Image[] sectorImages = null;
+    [SerializeField] RadarSector[] radarSectors = null;
     public List<GameObject> targets = new List<GameObject>();
     Dictionary<int, float> sectorIntensities = new Dictionary<int, float>();
+
 
 
     //param
     public float radarRange = 20;
     public float fadePerSecond = 0.3f;
-    public float arbitraryTestValue = .4f;
-    public float radarAccuracy = 23f; //how far off can the direction-of-arrival be, in degrees.
+    public float risePerSecond = 1.0f;
+    public float signalIntensity;
+    public float radarAccuracy; //how far off can the direction-of-arrival be, in degrees.
 
     //hood
 
@@ -28,10 +30,19 @@ public class RadarScreen : MonoBehaviour
     {
         ut = FindObjectOfType<UnitTracker>();
         player = GameObject.FindGameObjectWithTag("Player"); //TODO multiplayer this
-        ResetAllSectorsToZero();
+        //ResetAllSectorsToZero();
         PopulateSectorIntensitieswithZero();
+
     }
 
+    private void SetFadeTimeInEachSector()
+    {
+        foreach (RadarSector rs in radarSectors)
+        {
+            rs.fadeRate = fadePerSecond;
+            rs.riseRate = risePerSecond;
+        }
+    }
 
     private void PopulateSectorIntensitieswithZero()
     {
@@ -41,50 +52,65 @@ public class RadarScreen : MonoBehaviour
         }
     }
 
-    private void ResetAllSectorsToZero()
-    {
-        foreach (Image image in sectorImages)
-        {
-            image.color = Color.clear;
-        }
-    }
 
 
     // Update is called once per frame
     void Update()
     {
+        SetFadeTimeInEachSector();
+        ResetSectorIntensityToZero();
         GetTargets();
-        GetNoiseFromTargetsInEachSectors();
-        ConstantIntensityDecreaseForEachSector();
-        TurnIntensityIntoImageAdjustment();
+        IncreaseIntensityFromNoiseInEachSector();
+        //ConstantIntensityDecreaseForEachSector();
+        AssignCurrentIntensityToEachSector();
+        //TurnIntensityIntoImageAdjustment();
     }
 
-    private void TurnIntensityIntoImageAdjustment()
+    private void ResetSectorIntensityToZero()
     {
         for (int i = 0; i < 8; i++)
         {
-            sectorImages[i].color = new Color(1,1,1,sectorIntensities[i]);
+            sectorIntensities[i] = 0;
         }
     }
 
-    private void GetNoiseFromTargetsInEachSectors()
+    private void AssignCurrentIntensityToEachSector()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            radarSectors[i].intensityTarget = sectorIntensities[i];
+        }
+    }
+
+    private void IncreaseIntensityFromNoiseInEachSector()
     {
         foreach (GameObject target in targets)
         {
             Vector3 dir = target.transform.position - player.transform.position;
-            float signedAngFromNorth = Vector3.SignedAngle(dir, Vector3.up, Vector3.forward);
+            float signedAngFromNorth = Vector3.SignedAngle(dir, Vector3.up, Vector3.forward) - 22.5f ;
             if (signedAngFromNorth < 0)
             {
                 signedAngFromNorth += 360;
             }
 
+            //float randomSpread = 0;
             float randomSpread = UnityEngine.Random.Range(-radarAccuracy, radarAccuracy);
             signedAngFromNorth += randomSpread;
 
-            float approxSector = signedAngFromNorth / 45;
+            float approxSector = (signedAngFromNorth /45 );
             int sector = Mathf.RoundToInt(approxSector);
-            //Get the current target's noise and add it instead of the arbitrary value;
-            sectorIntensities[sector-1] += arbitraryTestValue;
+
+            if (sector >= 8)
+            {
+                sector = 0;
+            }
+            if (sector < 0)
+            {
+                sector = 7;
+            }
+            Debug.Log("angleFromNorth: " + signedAngFromNorth + " goes into approxSector: " + approxSector + " rounds to: " + sector);
+            //TODO: Get the current target's noise and add it instead of the arbitrary value;
+            sectorIntensities[sector] = sectorIntensities[sector] + signalIntensity;
         }
 
     }
@@ -100,7 +126,6 @@ public class RadarScreen : MonoBehaviour
         {
             sectorIntensities[i] -= fadePerSecond * Time.deltaTime;
             sectorIntensities[i] = Mathf.Clamp01(sectorIntensities[i]);
-
         }
     }
 }
