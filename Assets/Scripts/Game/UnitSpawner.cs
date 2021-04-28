f7eb196bf668bad56ddb6a4c320a05d4601edff0
jclayton128 <jclayton128@gmail.com>
2021-04-27 23:39:04 -0400

    Unit spawn panel, build queue, rework of resolution and pixel perfect stuff.

--- Assets/Scripts/Game/UnitSpawner.cs
+++ Assets/Scripts/Game/UnitSpawner.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitSpawner : MonoBehaviour
 {
@@ -9,9 +10,18 @@ public class UnitSpawner : MonoBehaviour
     UnitTracker ut;
     AllegianceManager am;
     CityManager cm;
-    [SerializeField] SpawnPanel sp = null;
-    [SerializeField] GameObject[] unitPrefabMenu = null;
+    List<GameObject> unitsInQueue = new List<GameObject>();
+    [SerializeField] Image[] queueStack = null;
+    [SerializeField] Sprite[] unitIcons = null;
+    [SerializeField] GameObject[] unitPrefabMenu = null;  //soldier, tank, missile truck, helo
     GameObject playerAtComputer;
+
+    //param
+    float timeBetweenSpawningUnits = 5f; //seconds
+    int[] unitCosts = new int[] { 10, 50, 30, 100 };
+
+    //hood
+    float timeSinceLastSpawn = 0;
 
     void Start()
     {
@@ -24,30 +34,97 @@ public class UnitSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ListenForCheatSummon();
+        ListenForKeyboardSummon();
+        MeasureTimeForQueue();
     }
 
-    private void ListenForCheatSummon()
+    private void MeasureTimeForQueue()
     {
-        if (Input.GetKeyUp(KeyCode.Alpha1))
+        if (unitsInQueue.Count > 0)
         {
-            GameObject factionLeader = am.GetFactionLeader(am.GetPlayerIFF()).gameObject;
-            CitySquare closestCSToPlayer = cm.FindNearestCitySquare(factionLeader.transform.transform, am.GetPlayerIFF());
-            GameObject unit = Instantiate(unitPrefabMenu[0], closestCSToPlayer.transform.position, Quaternion.identity) as GameObject;
-            unit.GetComponentInChildren<IFF>().SetIFFAllegiance(am.GetPlayerIFF());
+            timeSinceLastSpawn += Time.deltaTime;
+            if (timeSinceLastSpawn >= timeBetweenSpawningUnits)
+            {
+                SpawnNextUnitInQueue();
+                timeSinceLastSpawn = 0;
+            }
         }
     }
 
-    public void PlayerSpawnUnit(int chosenUnit)
+    private void ListenForKeyboardSummon()
     {
-        if (!sp.isExtended) { return; }
-        int iffToSpawnWith = playerAtComputer.GetComponentInChildren<IFF>().GetIFFAllegiance();
-        Vector3 spawnPos = cm.FindNearestCitySquare(playerAtComputer.transform, iffToSpawnWith).transform.position;
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            PlayerOrderUpNewUnit(0);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            PlayerOrderUpNewUnit(1);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            PlayerOrderUpNewUnit(2);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            PlayerOrderUpNewUnit(3);
+        }
+    }
 
-        GameObject newUnit = Instantiate(unitPrefabMenu[chosenUnit], spawnPos, Quaternion.identity) as GameObject;
-        newUnit.GetComponentInChildren<IFF>().SetIFFAllegiance(iffToSpawnWith);
-        ut.AddUnitToTargetableList(newUnit);
-        return;
+    public void PlayerOrderUpNewUnit(int chosenUnit)
+    {
+        //TODO check for sufficient funds
+
+        if (unitsInQueue.Count >= queueStack.Length)
+        {
+            Debug.Log("queue is full!");
+            //TODO play a 'bounce' sound to prevent ordering while the queue is full
+            return;
+        }
+        else
+        {
+            GameObject newUnit = unitPrefabMenu[chosenUnit];
+            int iffToSpawnWith = playerAtComputer.GetComponentInChildren<IFF>().GetIFFAllegiance();
+            newUnit.GetComponentInChildren<IFF>().SetIFFAllegiance(iffToSpawnWith);
+            unitsInQueue.Add(newUnit);
+            AddIconToQueueStack(unitIcons[chosenUnit]);
+        }
+        
+        //TODO withdraw unit costs from MoneyHolder
+
+    }
+
+    private void SpawnNextUnitInQueue()
+    {
+        GameObject unitBlueprint = unitsInQueue[0];
+        int iffToSpawnWith = unitBlueprint.GetComponentInChildren<IFF>().GetIFFAllegiance();
+        Vector3 spawnPos = cm.FindNearestCitySquare(playerAtComputer.transform, iffToSpawnWith).transform.position;
+        GameObject newActualUnit = Instantiate(unitBlueprint, spawnPos, Quaternion.identity) as GameObject;
+        Debug.Log("Made a new unit: " + newActualUnit.name);
+        ut.AddUnitToTargetableList(newActualUnit);
+        unitsInQueue.RemoveAt(0);
+        DecrementQueueStack();      
+    }
+
+    private void AddIconToQueueStack(Sprite newIcon)
+    {
+        int openSpot = unitsInQueue.Count;
+        queueStack[openSpot-1].sprite = newIcon;
+
+    }
+
+    private void DecrementQueueStack()
+    {
+        if (unitsInQueue.Count == 0)
+        {
+            queueStack[0].sprite = null;
+            return;
+        }
+        for (int i = 0; i < unitsInQueue.Count-1; i++)
+        {
+            queueStack[i].sprite = queueStack[i + 1].sprite;
+        }
+        queueStack[unitsInQueue.Count].sprite = null;
     }
 
     public void AISpawnUnit(int chosenUnit, int unitIFF)
